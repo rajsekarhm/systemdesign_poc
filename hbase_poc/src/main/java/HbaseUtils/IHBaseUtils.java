1680a228c894ceac64aba05ecb7aa4394c8f1933
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-08-17 11:34:11 +0530

    hbase poc

---
+++ hbase_poc/src/main/java/HbaseUtils/IHBaseUtils.java
@@ -0,0 +1,9 @@
+package HbaseUtils;
+
+public interface IHBaseUtils {
+
+    public  void put(String rowKey, String value, String columnFamily, String column) throws Exception;
+    public  byte[] get(String rowKey,String columnFamily, String column)throws  Exception;
+    public void scan(String columnFamily,String column) throws  Exception;
+    public  void delete(String rowKey,String columnFamily,String column) throws  Exception;
+}
