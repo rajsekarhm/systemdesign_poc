c9c6e272b14e32cb6642177072e502e017d0e28e
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-09-07 13:39:31 +0530

    code revamp

--- mail_ftp/mailer/mailer.js
+++ mail_ftp/mailer/mailer.js
@@ -1,18 +1,26 @@
-const { service } = require("../mailProperties");
+const { mailServices } = require("../mailProperties");
 const nodeMailer = require("nodemailer");
-const { userMail, password } = service.google;
+const {
+  userMail,
+  password,
+  service: _service,
+  host: _host,
+  port: _port,
+} = mailServices.testAccount;
+
+console.log(_host, _port, userMail, password);
 let transporter = nodeMailer.createTransport({
-  host: "smtp.gmail.com",
-  service: "gmail",
-  secure: true,
-  port: 587,
+  host: _host,
+  service: _service,
+  secure: false,
+  port: _port,
   auth: {
     user: userMail,
     pass: password,
   },
 });
 
-export function sentMail(email, content) {
+function sentMail(email, content) {
   return new Promise((resolve, reject) => {
     const mailOptions = {
       from: userMail,
@@ -24,6 +32,7 @@ export function sentMail(email, content) {
     };
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
+        console.log(err);
         reject(err);
       } else {
         resolve({ message: "Mail Sent Successfully", info: info });
@@ -31,3 +40,37 @@ export function sentMail(email, content) {
     });
   });
 }
+
+const _content = `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Password Reset</title>
+</head>
+<body>
+  <p>Dear [User's Name],</p>
+
+  <p>We received a request to reset your account password. If you made this request, please click the link below to reset your password:</p>
+
+  <p><a href="[Reset Link]" target="_blank" style="color: #1a73e8; text-decoration: none;">Reset My Password</a></p>
+
+  <p>If you did not request a password reset, please ignore this email. Your password will remain unchanged.</p>
+
+  <p>For security reasons, this link will expire in 24 hours.</p>
+
+  <p>If you need further assistance, please contact our support team.</p>
+
+  <p>Best regards,<br>
+  [Your Company Name]</p>
+
+  <hr>
+
+  <p style="font-size: 12px; color: #777;">If you’re having trouble clicking the "Reset My Password" button, copy and paste the URL below into your web browser:</p>
+  <p style="font-size: 12px; color: #777;">[Reset Link]</p>
+</body>
+</html>
+`;
+
+module.exports = {
+  sentMail,
+};
